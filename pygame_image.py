ffbd5cc6ac8c32eb1bc2feae3dd4a49c8d61ce6a
zaki <c0a23180@edu.teu.ac.jp>
2024-06-11 16:21:39 +0900

    練習6：こうかとんはワープを覚えた

--- pygame_image.py
+++ pygame_image.py
@@ -17,13 +17,14 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        kk_rct = kk_img.get_rect #こうかとんRectの抽出
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
+        kk_rct = kk_img.get_rect() #こうかとんRectの抽出
         kk_rct.center = 300,200
         screen.blit(kk_img,kk_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
