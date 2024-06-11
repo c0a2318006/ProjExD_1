45663f3e5fdb173dcc022999281ccea4ef177934
zaki <c0a23180@edu.teu.ac.jp>
2024-06-11 17:43:13 +0900

    演習課題1：こうかとんの速度調整

--- pygame_image.py
+++ pygame_image.py
@@ -1,31 +1,53 @@
 import os
 import sys
 import pygame as pg
-
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.png") #左向きの画像なので左右反転する
-    kk_img = pg.transform.flip(kk_img,True,False)
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()  # こうかとんRectの抽出
+    kk_rct.center = 300, 200    
     tmr = 0
+
+    bg_speed = 1
+    kk_speed = 2
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr%800
+        x = tmr%3200
         screen.blit(bg_img, [-x, 0])
-        kk_rct = kk_img.get_rect() #こうかとんRectの抽出
-        kk_rct.center = 300,200
-        screen.blit(kk_img,kk_rct)
-        pg.display.update()
-        tmr += 1        
-        clock.tick(200)
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0]) 
+        
 
+        key_lst = pg.key.get_pressed()  # 全キーの押下状態を取得
+        
+        if key_lst[pg.K_UP]:  # 上矢印キーが押されたら
+            kk_rct.move_ip(0, -1)
+        if key_lst[pg.K_DOWN]:  # 下矢印キーが押されたら
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:  # 左矢印キーが押されたら
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:  # 右矢印キーが押されたら
+            kk_rct.move_ip(+1*2, 0)
+
+        if not (key_lst[pg.K_UP] or key_lst[pg.K_DOWN] or key_lst[pg.K_LEFT] or key_lst[pg.K_RIGHT]):
+            kk_rct.move_ip(-bg_speed, 0)
+
+
+        screen.blit(kk_img, kk_rct)  # kk_imgをkk_rctの設定にしたがって貼り付け
+
+        pg.display.update()
+        tmr += 1      
+        clock.tick(200)
 
 if __name__ == "__main__":
     pg.init()
